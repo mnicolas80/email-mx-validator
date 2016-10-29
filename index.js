a7425f39c0266decf559d2628879af67d4ae51c3
Nick <mnicolas80@gmail.com>
2016-10-29 09:02:33 +0300

    Initial commit for version 1.0.0

---
+++ index.js
@@ -0,0 +1,26 @@
+// Regular expression source: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+var dns = require('dns');
+var valid = false;
+var regEx = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+exports.validEmail = function validEmail(email, cb) {
+  // Validate email syntax
+  if (!email.match(regEx)) {
+    valid = false;
+    cb(valid);
+    return;
+  }  
+
+  // Validate domain MX records
+  var domain = email.split('@')[1];  
+  dns.resolve(domain, 'MX', (err, addresses) => {    
+    if (err) {
+      valid = false;      
+    } else if (addresses && addresses.length > 0) {      
+      valid = true;
+    } else {
+      valid = false;
+    }
+    cb(valid);    
+  });
+}
